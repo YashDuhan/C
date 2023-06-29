09723eac6bfd70c71cbde2244ddf5b6127e9332b
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2023-06-29 18:46:50 +0530

    java_prep

---
+++ java_prep/hello_world.java
@@ -0,0 +1,10 @@
+package java_prep;
+
+public class hello_world {
+	public static void main(String args[]) {
+		// If we use println then java will automatically add a new line but not in normal print
+		System.out.println("Hello world");
+		System.out.print("Hello world");
+		System.out.println("Hello world");
+	}
+}
