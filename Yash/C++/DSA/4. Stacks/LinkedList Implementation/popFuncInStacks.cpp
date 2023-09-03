42dece50728e039bc7084f15ea70d49fc61aef71
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2023-09-03 22:32:58 +0530

    Practiced Stacks(LinkedList Implementation)

---
+++ Yash/C++/DSA/4. Stacks/LinkedList Implementation/popFuncInStacks.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next= nullptr;
+    }
+};
+
+class Stack
+{
+    private:
+    Node *head = nullptr;
+    public:
+    void push(int x)
+    {
+        Node *temp = new Node(x);
+        temp->next = head;
+        head = temp;
+    }
+    void display()
+    {
+        Node *temp = head;
+        while(temp != nullptr)
+        {
+            cout<<temp->data<<endl;
+            temp = temp->next;
+        }
+    }
+    int top()
+    {
+        return head->data;
+    }
+    void pop()
+    {
+        head = head->next;
+    }
+
+};
+
+int main()
+{
+    Stack s1;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(40);
+    s1.push(50);
+
+    s1.display();
+    cout<<"Element popped :"<<endl;
+    s1.pop();
+    s1.display();
+}
---
+++ Yash/C++/DSA/4. Stacks/LinkedList Implementation/topFuncInStacks.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next= nullptr;
+    }
+};
+
+class Stack
+{
+    private:
+    Node *head = nullptr;
+    public:
+    void push(int x)
+    {
+        Node *temp = new Node(x);
+        temp->next = head;
+        head = temp;
+    }
+    void display()
+    {
+        Node *temp = head;
+        while(temp != nullptr)
+        {
+            cout<<temp->data<<endl;
+            temp = temp->next;
+        }
+    }
+    int top()
+    {
+        return head->data;
+    }
+
+};
+
+int main()
+{
+    Stack s1;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(40);
+    s1.push(50);
+
+    s1.display();
+    cout<<"The top most element is "<<s1.top()<<endl;
+}
