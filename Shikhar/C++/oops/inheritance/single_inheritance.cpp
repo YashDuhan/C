b490f5bb28563a714dc5cc147ecf8a6057cac5b4
Shikhar Gupta <shikhargupta0645@gmail.com>
2023-09-12 20:47:00 +0530

    Update single_inheritance.cpp

--- Shikhar/C++/oops/inheritance/single_inheritance.cpp
+++ Shikhar/C++/oops/inheritance/single_inheritance.cpp
@@ -11,8 +11,44 @@ public:
     int getData2();
 };
 
+void Base ::setData(void)
+{
+    data1 = 10;
+    data2 = 20;
+}
+int Base ::getData1()
+{
+    return data1;
+}
+int Base ::getData2()
+{
+    return data2;
+}
+
+class Derived : public Base // class is being derived publically
+{
+    int data3;
+
+public:
+    void process();
+    void display();
+};
+
+void Derived ::process()
+{
+    data3 = data2 * getData1();
+}
+void Derived ::display()
+{
+    cout << "value of data 1 is : " << getData1() << endl;
+    cout << "value of data 2 is : " << data2 << endl;
+    cout << "value of data 3 is : " << data3 << endl;
+}
 int main()
 {
-
+    Derived der;
+    der.setData();
+    der.process();
+    der.display();
     return 0;
 }
