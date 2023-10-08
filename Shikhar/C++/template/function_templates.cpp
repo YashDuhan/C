cf0c3298faf7f9f3f25b90c65ce68303a0ecb466
Shikhar Gupta <shikhargupta0645@gmail.com>
2023-10-08 19:35:27 +0530

    Update function_templates.cpp

--- Shikhar/C++/template/function_templates.cpp
+++ Shikhar/C++/template/function_templates.cpp
@@ -9,10 +9,23 @@ float funcAverage(T1 a, T2 b)
     return avg;
 }
 
+template<class T>
+void swapp(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;   
+}
 int main(){
     float a;
     a = funcAverage(5, 2.43);
     cout<<"The average of the numbers is: "<<a;
-    
+
+    int x = 5, y = 7;
+    cout<<endl<<"1st number before swapping = "<<x<<endl;
+    cout<<"2nd number before swapping = "<<y<<endl;
+    swapp(x,y);
+    cout<<endl<<"1st number after swapping = "<<x<<endl;
+    cout<<"2nd number after swapping = "<<y<<endl;
     return 0;
 }
