bc03627ddff9bd7b2f9026909d74c8308ee38fa5
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2023-10-13 22:43:03 +0530

    Practiced Trees

---
+++ Yash/C++/DSA CWH/6. Tree/1. Binary Tree/intro.cpp
@@ -0,0 +1,22 @@
+/*
+Binary Tree : A binary tree is a tree data structure where each node has, at most, two child nodes.
+(Nodes can't have more than 2 childs)
+
+Visual Representation  of Binary Tree
+
+                                100 (Root)
+                              /          \
+                             /            \
+                            /              \
+                           /                \
+                     20 (Node1)         30 (Node2)
+                    /   \               /   \
+                   /     \             /     \
+                  /       \           /       \
+            21 (Node3)  22 (Node4) 31 (Node5) 32 (Node6)
+                                          / \
+                                         /   \
+                                        /     \
+                                40 (Node7)   15 (Node8)
+
+*/
