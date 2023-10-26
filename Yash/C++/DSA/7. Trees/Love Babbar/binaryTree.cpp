7b677e16e5ed8bbb10751cea1e726621201153eb
Yash Duhan <yash2004duhan@gmail.com>
2023-10-26 05:43:41 +0530

    practiced Binary Tree

---
+++ Yash/C++/DSA/7. Trees/Love Babbar/binaryTree.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+class Node
+{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *buildTree(Node *root)
+{
+    cout << "Enter data for node : " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if(data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter data to be inserted in left of : " << data <<  endl;
+    root->left = buildTree(root->left); //Recursion is being used here
+    cout <<"Enter data to be inserted in right  of : " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+
+int main()
+{
+    Node *root = NULL;
+    buildTree(root);
+}
