0a5aa103d17af2a1a858c440bc2709dda29e8b07
Yash Duhan <yash2004duhan@gmail.com>
2024-02-16 15:47:30 +0530

    Count Number of SubSets With Given Diff

---
+++ Yash/C++/DSA/9. DP/Aditya Verma/KnapSack Problems/Part11_CountNumberOfSubSetWithGivenDiff.cpp
@@ -0,0 +1,12 @@
+/*
+ this problem is just like the previous problem but this time we are given the difference between 2 subsets,
+
+ //Array is {1,1,2,3}
+ //Given difference is 1
+ //O/P is 3
+ Now it can have multiple subsets
+    i)   {1,3} & {1,2} ---> Sum of Difference is 1
+    ii)  {1,3} & {1,2} ---> Sum of Difference is 1(The array had two 1s)
+    iii) {1,1,2} & {3} ---> Sum of Difference is 1
+
+ */
